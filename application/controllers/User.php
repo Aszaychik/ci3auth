ae5a53d0908488aa3e11fd6fe133b8767c552413
AsZaychik <aszaychik@gmail.com>
2023-01-18 05:06:38 +0700

    User View

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -13,8 +13,14 @@ class User extends CI_Controller {
 		$data['user'] = $this->db->get_where('users', [
 			'email' => $this->session->userdata('email')
 		])->row_array();
-		$username = $data['user']['name'];
-		echo "<b>Welcome $username</b>";
+
+		$data['title'] = $data['user']['name'];
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/sidebar', $data);
+		$this->load->view('templates/topbar', $data);
+		$this->load->view('user/index', $data);
+		$this->load->view('templates/footer');
 	}
 }
 ?>
---
+++ application/views/user/index.php
@@ -0,0 +1,32 @@
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">My Profile</h1>
+										
+										<div class="card mb-3" style="max-width: 540px;">
+										<div class="row no-gutters">
+											<?php 
+												if(empty($user['image'])){
+														$image = 'https://source.unsplash.com/random/128x128';
+												}else{
+														$image = base_url('assets/img/') . $user['image'];
+												}
+												?>
+											<div class="col-md-4" style="background-image: url('<?= $image; ?>'); background-repeat: no-repeat; background-size: cover; background-position: center;">
+											</div>
+											<div class="col-md-8">
+												<div class="card-body">
+													<h5 class="card-title"><?= $user['name'];?></h5>
+													<p class="card-text"><?= $user['email'];?></p>
+													<p class="card-text"><small class="text-muted">Member since <?= date('d F Y', $user['date_created']);?></small></p>
+												</div>
+											</div>
+										</div>
+									</div>
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
